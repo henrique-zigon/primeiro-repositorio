d7549a917924fa768a4abfa57e6ea041926670f8
henrique-zigon <61800698+henrique-zigon@users.noreply.github.com>
2020-11-11 17:25:27 -0300

    eee
    euuu

---
+++ Calculo/script do desafio(1).R
@@ -0,0 +1,21 @@
+set.seed(999)
+n<-6048
+
+#-----------------IDADE--------------------------------
+faixaIdade<-abs(round(rnorm(n,23,5),0))
+
+data.default<-data.frame(table(faixaIdade))
+data.default<-data.frame(id=(1:length(data.default$Freq)),data.default)
+
+freq_relativa<-round((data.default$Freq/sum(data.default$Freq)*100),2)
+
+data.freq<-data.frame(freq_relativa)
+data.freq<-data.frame(id=(1:length(data.freq$freq_relativa)),freq_relativa)
+
+tabelaFinal<-data.frame(merge(data.default,data.freq))
+
+barplot(tabelaFinal$freq_relativa,xlab= "idade",ylab="Frequência_Relativa")
+barplot(prop.table(tabelaFinal$freq_relativa),xlab="idade",ylab="Frequência
++ Relativa",col=c("red","blue"))
+
+#-----------------RENDA MENSAL----------------------
---
+++ Calculo/script do desafio.R
@@ -0,0 +1,61 @@
+meses<-c(1:10)
+preços<-c(60,65,85,50,56,90,58,88,55,70)
+tabela<-data.frame(meses,preços)
+#criando tabela
+
+
+
+tabela
+#plota tabela
+plot(tabela$meses,tabela$preços)
+x<-(lm(tabela$preços~tabela$meses))
+summary(x)#Achar equação=0.5636*x+64.6000(coefficients)
+equacao<-0.5636*11+64.6
+
+#coloca uma reta de previsão em cima do grafico anterior
+abline(lm(tabela$preços~tabela$meses))
+
+#mostra gráfico de residuos
+plot(lm(tabela$meses~tabela$preços))
+#------------------------------------------------
+
+#criando tabela com previsão
+meses1<-c(1:11)
+preços1<-c(60,65,85,50,56,90,58,88,55,70,equacao)
+tabela1<-data.frame(meses1,preços1)
+
+#plota tabela1
+plot(tabela1$meses1,tabela1$preços1)
+
+#coloca reta
+abline(lm(tabela1$preços1~tabela1$meses1))
+
+#-------------------------------------------------
+
+#criando tabela para previsão
+preços2<-c(70,70.79,75.86)    #dados calculados no excel(ultimo é erro padrão)
+meses2<-c("Oct","Nov-Atual","Nov-forecast")          
+tabela2<-data.frame(meses2,preços2)
+
+#plotar gráfico de barra da previsão
+barplot(tabela2$preços2,names.arg= tabela2$meses2,ylab="$")
+
+#--------------------------------------------------
+
+#criando variaveis
+ec2<-44.5356
+EBS<-14.9031
+s3<-11.3512
+preçosSeparados<-c(ec2,EBS,s3)
+pie(preçosSeparados)
+
+#----------------------------------------------------
+
+#Juros do billing
+#Levando em consideração que o montante final seja o mesmo o que o estipulado
+montante1mes<-equacao*(1+0.1)**1
+montante1mes
+#previsao de dezembro
+equacao1<-0.5636*12+64.6
+montante2mes<-(montante1mes*(1+0.1)**2)+equacao1
+montante2mes
